7ebea0abd7a11bc51b9dc4c356571cb73251d592
4kmalm <16521070@mahasiswa.itb.ac.id>
2023-05-06 21:41:22 +0700

    feat: add Customer package
    Ada kelas Customer ada kelas member

---
+++ src/main/java/ooploverz/tubes2_oop/customer/Member.java
@@ -0,0 +1,31 @@
+package ooploverz.tubes2_oop.customer;
+
+import lombok.*;
+
+@ToString
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class Member extends Customer{
+    private final float discoutVIP = 0.1f;
+    @NonNull @Getter @Setter
+    private String name;
+    @NonNull @Getter @Setter
+    private String phoneNumber;
+    @Getter @Setter
+    private int points = 0;
+    @Getter @Setter
+    private boolean isVIP = false;
+
+    public int priceCuts(int price) {
+        int afterDiscount = discountCuts(hargaBarang);
+        int pointUsed = Math.min(afterDiscount, points);
+        int resultCuts = afterDiscount - pointUsed;
+        this.points -= pointUsed;
+        return resultCuts;
+    }
+
+    protected int discountCuts(int price) {
+        return this.isVIP ? (int) Math.floor(price - (price * discoutVIP)) : price;
+    }
+}
+
