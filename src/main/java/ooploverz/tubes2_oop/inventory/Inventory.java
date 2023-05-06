3a68a0f5a05b45e9db2f8b9e24b3cd37d6969f3c
Ghazi Akmal Fauzan <13521058@std.stei.itb.ac.id>
2023-05-06 20:58:27 +0700

    Create Inventory.java

---
+++ src/main/java/ooploverz/tubes2_oop/inventory/Inventory.java
@@ -0,0 +1,171 @@
+package ooploverz.tubes2_oop.inventory;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Inventory {
+    private Item[] listItem;
+
+    public void addItem(int stock, String name, int price, int buyPrice, String category, String image){
+        Item[] temp = new Item[listItem.length + 1];
+        System.arraycopy(listItem, 0, temp, 0, listItem.length);
+        temp[listItem.length] = new Item(stock, name, price, buyPrice, category, image);
+        listItem = temp;
+    }
+
+    public void removeItem(String name){
+        Item[] temp = new Item[listItem.length - 1];
+        int index = 0;
+        for (Item i : listItem) {
+            if (!i.getName().equals(name)) {
+                temp[index] = i;
+                index++;
+            }
+        }
+        listItem = temp;
+    }
+
+    public Item getItem(String name){
+        for (Item i : listItem) {
+            if (i.getName().equals(name)) {
+                return i;
+            }
+        }
+        return null;
+    }
+
+    public int getStock(String name){
+        for (Item i : listItem) {
+            if (i.getName().equals(name)) {
+                return i.getStock();
+            }
+        }
+        return 0;
+    }
+
+    public int getPrice(String name){
+        for (Item i : listItem) {
+            if (i.getName().equals(name)) {
+                return i.getPrice();
+            }
+        }
+        return 0;
+    }
+
+    public int getBuyPrice(String name){
+        for (Item i : listItem) {
+            if (i.getName().equals(name)) {
+                return i.getBuyPrice();
+            }
+        }
+        return 0;
+    }
+
+    public String getCategory(String name){
+        for (Item i : listItem) {
+            if (i.getName().equals(name)) {
+                return i.getCategory();
+            }
+        }
+        return null;
+    }
+
+    public String getImage(String name){
+        for (Item i : listItem) {
+            if (i.getName().equals(name)) {
+                return i.getImage();
+            }
+        }
+        return null;
+    }
+
+    public void addStock(String name){
+        for (Item i : listItem) {
+            if (i.getName().equals(name)) {
+                i.setStock(i.getStock() + 1);
+            }
+        }
+    }
+
+    public void addStock(String name, int stock){
+        for (Item i : listItem) {
+            if (i.getName().equals(name)) {
+                i.setStock(i.getStock() + stock);
+            }
+        }
+    }
+
+    public void removeStock(String name){
+        for (Item i : listItem) {
+            if (i.getName().equals(name)) {
+                i.setStock(i.getStock() - 1);
+            }
+        }
+    }
+
+    public void removeStock(String name, int stock){
+        for (Item i : listItem) {
+            if (i.getName().equals(name)) {
+                i.setStock(i.getStock() - stock);
+            }
+        }
+    }
+
+    public void setStock(String name, int stock){
+        for (Item i : listItem) {
+            if (i.getName().equals(name)) {
+                i.setStock(stock);
+            }
+        }
+    }
+
+    public void changeName(String name, String newName){
+        for (Item i : listItem) {
+            if (i.getName().equals(name)) {
+                i.setName(newName);
+            }
+        }
+    }
+
+    public void setPrice(String name, int price){
+        for (Item i : listItem) {
+            if (i.getName().equals(name)) {
+                i.setPrice(price);
+            }
+        }
+    }
+
+    public void setBuyPrice(String name, int buyPrice){
+        for (Item i : listItem) {
+            if (i.getName().equals(name)) {
+                i.setBuyPrice(buyPrice);
+            }
+        }
+    }
+
+    public void setCategory(String name, String category){
+        for (Item i : listItem) {
+            if (i.getName().equals(name)) {
+                i.setCategory(category);
+            }
+        }
+    }
+
+    public void setImage(String name, String image){
+        for (Item i : listItem) {
+            if (i.getName().equals(name)) {
+                i.setImage(image);
+            }
+        }
+    }
+
+    public int getTotalItem(){
+        return listItem.length;
+    }
+}
