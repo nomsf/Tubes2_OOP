5f69046509d4b5abe26a59fd7a9445042fa110a4
fadhilAmri <fadhil133113@gmail.com>
2023-05-06 10:45:49 +0700

    Fixing navbar

--- src/main/java/ooploverz/tubes2_oop/HalamanUtama.java
+++ src/main/java/ooploverz/tubes2_oop/HalamanUtama.java
@@ -11,6 +11,8 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ContentDisplay;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.GridPane;
