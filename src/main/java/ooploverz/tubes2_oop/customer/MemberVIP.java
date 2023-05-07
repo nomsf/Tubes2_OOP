e8a5b38ce2a422d6d56172993e692792c2f048d1
akmaldika <13521070@mahasiswa.itb.ac.id>
2023-05-07 15:45:19 +0700

    feat : add main program test case

--- src/main/java/ooploverz/tubes2_oop/customer/MemberVIP.java
+++ src/main/java/ooploverz/tubes2_oop/customer/MemberVIP.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @AllArgsConstructor
 public class MemberVIP extends Member {
 
@@ -14,10 +16,61 @@ public class MemberVIP extends Member {
 
     @Override
     public int priceCuts(int totalPrice) {
-        if (this.isActive) return totalPrice;
+        if (!this.isActive) return totalPrice;
 
         float discountVIP = 0.1f;
         int afterDisc = (int) Math.floor(totalPrice - (totalPrice * discountVIP)) ;
         return super.priceCuts(afterDisc);
+    }
+
+    public static void main(String[] args) {
+        // create a new member instance
+        MemberVIP member = new MemberVIP(1, "John Doe", "555-1234", 100, true);
+
+        // test the getters and setters
+        System.out.println(member.getCustomerId()); // output: 1
+        System.out.println(member.getName()); // output: John Doe
+        System.out.println(member.getPhoneNumber()); // output: 555-1234
+        System.out.println(member.getPoints()); // output: 100
+        System.out.println(member.isActive()); // output: true
+
+        MemberVIP member2 = new MemberVIP();
+
+        System.out.println(member2.getCustomerId()); // output: 0
+        System.out.println(member2.getName()); // output: null
+        System.out.println(member2.getPhoneNumber()); // output: null
+        System.out.println(member2.getPoints()); // output: 0
+        System.out.println(member2.isActive()); // output: true
+
+        MemberVIP member4 = new MemberVIP();
+        System.out.println(member4.getCustomerId()); // output: 1
+        MemberVIP member5 = new MemberVIP();
+        System.out.println(member5.getCustomerId()); // output: 2
+
+        // test the priceCuts method
+        System.out.println("-------------------");
+        int totalPrice = 2000;
+        System.out.println(member.getPoints() + " " + member.isActive()); // output: 100 true
+        int discountedPrice = member.priceCuts(totalPrice);
+
+        System.out.println(discountedPrice); // output: 1700
+        System.out.println(member.getPoints()); // output: 0
+
+        member.setPoints(100);
+        discountedPrice = member.priceCuts(100);
+
+        System.out.println(discountedPrice); // output: 0
+        System.out.println(member.getPoints()); // output: 55
+
+        member.setPoints(100);
+        discountedPrice = member.priceCuts(100);
+
+        System.out.println(discountedPrice); // output: 0
+        System.out.println(member.getPoints()); // output: 10
+
+        // test the toListString method
+        List<String> memberList = member.toListString();
+
+        System.out.println(memberList); // output: [1, Jane Doe, 555-5678, 0, false]
     }
 }
